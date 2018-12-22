4adcd63176d02ccf1f42c2153c8e262e57c986e7
Regem Semilla <regem.semilla123@gmail.com>
2018-12-22 12:28:19 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react';
+import Particles from 'react-particles-js'; 
+import Clarifai from 'clarifai';
+import Navagation from './components/Navigation/Navigation';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import './App.css';
+
+const app = new Clarifai.App({
+ apiKey: "fbdfae1ba40c46dbaa99699f3bf4c30d"
+});
+
+
+const particlesOptions = {
+   particles: {
+      number: {
+        value: 100,
+        density: {
+          enable: true,
+          value_area: 800
+        }
+      }
+   }
+};
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageURL: '',
+      box: {}
+    }
+  }
+
+  calculateFaceLocation = (data) => {
+
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('imageinput');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    // console.log(clarifaiFace);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      righCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+
+    }
+  }
+
+
+  displayFaceBox = (box) => {
+    // console.log('ss')
+    this.setState({box:box});
+  }
+
+  onInputChange = (event) => {
+    // console.log('aa')
+   this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    // console.log('as')
+    this.setState({imageURL: this.state.input})
+   
+   app.models
+.predict(
+Clarifai.FACE_DETECT_MODEL,
+    // URL
+    this.state.input
+)
+.then(response => this.displayFaceBox(this.calculateFaceLocation(response))) 
+    // do something with responseconsole.log(response);
+.catch(err => console.log(err))
+
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <Particles className='particles'
+          params={particlesOptions}
+        />
+        <Navagation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+        <FaceRecognition box={this.state.box} imageURL={this.state.imageURL}/>
+      </div>
+    );
+  }
+}
+
+export default App;
